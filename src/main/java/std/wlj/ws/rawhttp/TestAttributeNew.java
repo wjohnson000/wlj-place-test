89af3dae2fbe0cd4b18ae28d292e2757d5d13225
wjohnson000 <wjohnson@familysearch.org>
2014-05-20 09:06:25 -0600

    New Test

--- src/main/java/std/wlj/ws/rawhttp/TestAttributeNew.java
+++ src/main/java/std/wlj/ws/rawhttp/TestAttributeNew.java
@@ -1,14 +1,18 @@
 package std.wlj.ws.rawhttp;
 
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.familysearch.standards.place.ws.model.LocalizedNameDescModel;
 import org.familysearch.standards.place.ws.model.RootModel;
+import org.familysearch.standards.place.ws.model.TypeModel;
 
 
 public class TestAttributeNew {
 
     /** Base URL of the application */
-    private static String baseUrl = "http://localhost:8080/std-ws-place/places";
+    private static String baseUrl = "http://localhost:8080/std-ws-place/places/attribute-types/";
 //    private static String baseUrl = "http://ec2-54-204-45-169.compute-1.amazonaws.com:8080/std-ws-place/places";
 
 
@@ -17,42 +21,74 @@ public class TestAttributeNew {
      */
     public static void main(String[] args) throws Exception {
         readAttributes();
-        readCitations();
+        TypeModel attrModel = createAttribute();
+        updateAttribute(attrModel);
     }
 
+    /**
+     * Read all attributes; read a specific attribute; attempt to read a non-existent attribute
+     * @throws Exception
+     */
     private static void readAttributes() throws Exception {
         URL url;
         RootModel model;
-        long time01 = 0;
-        long time02 = 0;
 
-        for (int i=0;  i<3;  i++) {
-            long nnow = System.nanoTime();
-            url = new URL(baseUrl + "/attribute-types/1010");
-            model = TestUtil.doGET(url);
-            time01 += (System.nanoTime() - nnow);
-            System.out.println("RM: " + model);
+        url = new URL(baseUrl);
+        model = TestUtil.doGET(url);
+        System.out.println("RM: " + model);
 
-            nnow = System.nanoTime();
-            url = new URL(baseUrl + "/attribute-types/1111" + i);
-            model = TestUtil.doGET(url);
-            time02 += (System.nanoTime() - nnow);
-            System.out.println("RM: " + model);
-        }
+        url = new URL(baseUrl + "1010");
+        model = TestUtil.doGET(url);
+        System.out.println("RM: " + model);
 
-        System.out.println("Time01: " + (time01 / 1000000.0));
-        System.out.println("Time02: " + (time02 / 1000000.0));
+        url = new URL(baseUrl + "2020");
+        model = TestUtil.doGET(url);
+        System.out.println("RM: " + model);
     }
 
-    private static void readCitations() throws Exception {
-//        URL url = new URL(baseUrl + "/citation-types/1055");
-//        RootModel model = TestUtil.getResults(url);
-//        System.out.println("RM: " + model);
-//
-//        for (int i=11111;  i<11114;  i++) {
-//            url = new URL(baseUrl + "/citation-types/" + i);
-//            model = TestUtil.getResults(url);
-//            System.out.println("RM: " + model);
-//        }
+    /**
+     * Create a new attribute type ...
+     * 
+     * @throws Exception
+     */
+    private static TypeModel createAttribute() throws Exception {
+        URL url = new URL(baseUrl);
+
+        LocalizedNameDescModel nameAndDesc = new LocalizedNameDescModel();
+        nameAndDesc.setLocale("en");
+        nameAndDesc.setName("WLJ-Attribute");
+        nameAndDesc.setDescription("My very own attribute ... how quaint ...");
+
+        List<LocalizedNameDescModel> nameAndDescList = new ArrayList<>();
+        nameAndDescList.add(nameAndDesc);
+
+        TypeModel theType = new TypeModel();
+        theType.setCode("WLJ_ZZZ");
+        theType.setIsPublished(true);
+        theType.setName(nameAndDescList);
+
+        RootModel inModel = new RootModel();
+        inModel.setType(theType);
+
+        RootModel outModel = TestUtil.doPOST(url, inModel);
+        System.out.println("RM01: " + outModel);
+        return outModel.getType();
+    }
+
+    private static void updateAttribute(TypeModel attrModel) throws Exception {
+        URL url = new URL(baseUrl + attrModel.getId());
+
+        LocalizedNameDescModel nameAndDesc = new LocalizedNameDescModel();
+        nameAndDesc.setLocale("ja");
+        nameAndDesc.setName("WLJ-Attribute-ja");
+        nameAndDesc.setDescription("My very own attribute ... how quaint ... ja");
+
+        attrModel.getLocalizedName().add(nameAndDesc);
+
+        RootModel inModel = new RootModel();
+        inModel.setType(attrModel);
+
+        RootModel outModel = TestUtil.doPUT(url, inModel);
+        System.out.println("RM02: " + outModel);
     }
 }
